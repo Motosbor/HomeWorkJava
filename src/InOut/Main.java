fdae06b265fcab656e8a30346b75d2c95363cd09
Motosbor <Lotsmanenkorr@mail.ru>
2019-02-28 11:41:01 +0400

    11

--- src/InOut/Main.java
+++ src/InOut/Main.java
@@ -1,7 +1,45 @@
 package InOut;
 
+import java.io.*;
+import java.util.LinkedList;
+
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        String s;
+        LinkedList<String> linkedList = new LinkedList<>();
+
+        try (BufferedReader reader = new BufferedReader(new FileReader("text"))) {
+
+            while ((s = reader.readLine()) != null) {
+                if(s.matches(".*([1-9][0-9])|.*[7-9]")) {
+                    String temp = s.toUpperCase();
+                    linkedList.add(temp);
+                }else {
+                    linkedList.add(s);
+                }
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        try (PrintWriter out = new PrintWriter(new FileWriter("text.txt"))) {
+
+            for (String string: linkedList) {
+
+                out.println(string);
+
+                }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        for (String string:linkedList) {
+            System.out.println(string);
+        }
     }
 }
