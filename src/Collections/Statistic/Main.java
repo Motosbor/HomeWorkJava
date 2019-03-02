2baa6f80efd9f3aef637d9a12a21a7dd82390529
Motosbor <lotsmanenkorr@mail.ru>
2019-03-02 19:01:50 +0400

    !!

--- src/Collections/Statistic/Main.java
+++ src/Collections/Statistic/Main.java
@@ -4,9 +4,7 @@ package Collections.Statistic;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
+import java.util.*;
 
 public class Main {
 
@@ -20,6 +18,12 @@ public class Main {
 
     }
 
+    public static void printTeam(Set<Team> teams){
+        for (Team t:teams) {
+            System.out.println("Name - " + t.getName() + " Country - " + t.getCountry() + " Season - " + t.getSeazon() + " Scores - " + t.getScores());
+        }
+    }
+
     public static void main(String[] args) {
 
         Team team = new Team("Eagle", "India", 3, 45);
@@ -29,6 +33,8 @@ public class Main {
         Team team4 = new Team("Bara", "Belgium", 6, 77);
         Team team5 = new Team("Cara", "Germany", 7, 111);
         Team team6 = new Team("Coro", "Java", 7, 111);
+
+
 
         ArrayList<Team> teamArrayList = new ArrayList<>();
         teamArrayList.add(team);
@@ -64,6 +70,50 @@ public class Main {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println("--------------");
+
+        HashSet<Team> teamSet = new HashSet<>();
+        teamSet.addAll(teamArrayList);
+        printTeam(teamSet);
+
+       Set<Team> sortedByCountryTeam = new TreeSet<Team>(new Comparator<Team>() {
+           @Override
+           public int compare(Team o1, Team o2) {
+               return o1.getCountry().compareTo(o2.getCountry());
+           }
+       });
+
+        Set<Team> sortedByName = new TreeSet<Team>(new Comparator<Team>() {
+            @Override
+            public int compare(Team o1, Team o2) {
+                int x = o1.getName().compareTo(o2.getName());
+                return x;
+            }
+        });
+
+        Set<Team> sortedByScore = new TreeSet<Team>(new Comparator<Team>() {
+            @Override
+            public int compare(Team o1, Team o2) {
+                if(o1.getScores() == o2.getScores()) return 0;
+                if(o1.getScores() > o2.getScores()) return 1;
+                else return -1;
+            }
+        });
+
+        System.out.println("--------------");
+        sortedByCountryTeam.addAll(teamSet);
+        printTeam(sortedByCountryTeam);
+        System.out.println("--------------");
+        sortedByScore.addAll(teamSet);
+        printTeam(sortedByScore);
+        System.out.println("--------------");
+        sortedByName.addAll(teamSet);
+        printTeam(sortedByName);
+
+
+
+
+
 
     }
 }
