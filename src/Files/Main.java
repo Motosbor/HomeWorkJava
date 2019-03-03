ca1560e8e585a627992b24851bbfaa9fd6a13d3e
Motosbor <lotsmanenkorr@mail.ru>
2019-03-03 21:53:11 +0400

    !!

--- src/Files/Main.java
+++ src/Files/Main.java
@@ -1,15 +1,22 @@
 package Files;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
+    public static int count = 1;
 
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Path p1 = Paths.get("C:\\Users\\Rus\\IdeaProjects\\HomeWorkJava\\Letter.txt");
         try {
 
@@ -30,10 +37,65 @@ public class Main {
 
         Path p2 = Paths.get("C:\\Users\\Rus\\IdeaProjects\\HomeWorkJava");
         try {
-            Files.walkFileTree(p2,new MyVisitor());
+            Files.walkFileTree(p2, new MyVisitor());
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-    }
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        List<String> strings = new ArrayList<>();
+
+        try {
+
+            String file = reader.readLine();
+
+            Path filePath = Paths.get(file);
+
+            if (Files.exists(filePath)) {
+
+                strings = Files.readAllLines(filePath);
+
+                List<String> copyStrings = new ArrayList<>();
+
+                for (String s : strings) {
+                    if (s.length() > 30) {
+                        copyStrings.add(s);
+                    }
+                }
+                file = reader.readLine();
+                filePath = Paths.get(file);
+
+                if (Files.exists(filePath)) {
+
+                    while (Files.exists(filePath)){
+
+                        String [] strings1 = file.split("\\(+\\d+\\)");
+                        strings1[0] = strings1[0] + "(" + count + ").";
+                        StringBuilder builder = new StringBuilder();
+                        for(String s : strings1) {
+                            builder.append(s);
+                        }
+                        file = builder.toString();
+                        filePath = Paths.get(file);
+                        count++;
+                    }
+
+                    Files.write(filePath, copyStrings, StandardOpenOption.CREATE_NEW);
+                    count++;
+
+                } else {
+                    Files.write(filePath, copyStrings, StandardOpenOption.CREATE_NEW);
+                }
+            }else {
+                System.out.println("Файла не существует");
+            }
+            } catch(IOException e){
+                e.printStackTrace();
+            }
+
+        }
+
 }
+
+
