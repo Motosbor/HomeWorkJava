e0ac27fdbcc8baee45e3360c32eb6dfd64026e9b
Motosbor <Lotsmanenkorr@mail.ru>
2019-03-05 14:22:32 +0400

    11

--- src/Collections/Airport/Airport.java
+++ src/Collections/Airport/Airport.java
@@ -13,11 +13,17 @@ public class Airport {
         this.airportMap = airportMap;
     }
 
-    public  void mapPut(Integer x, Passenger p){
-        LinkedList<Passenger> list = new LinkedList<>();
-        list.add(p);
-        this.airportMap.put(x,list);
+    public  void mapPut(LinkedList<Passenger> passengersList){
 
+        for (Passenger p :passengersList) {
+            if( this.airportMap.containsKey(p.getFlight())) {
+                this.airportMap.get(p.getFlight()).add(p);
+            }else {
+                LinkedList<Passenger> list = new LinkedList<>();
+                list.add(p);
+                this.airportMap.put(p.getFlight(),list);
+            }
+        }
     }
 
     public TreeMap<Integer, LinkedList<Passenger>> getAirportMap() {
--- src/Collections/Airport/Main.java
+++ src/Collections/Airport/Main.java
@@ -22,82 +22,19 @@ public class Main {
         }
     }
 
+    public static void printMap(Airport airport){
+
+        for(Map.Entry<Integer,LinkedList<Passenger>> item : airport.getAirportMap().entrySet()){
+            System.out.println(item.getKey());
+            Collections.sort(item.getValue(), new FlightsCompare());
+            passengerList(item.getValue());
+
+        }
+    }
+
     public static void main(String[] args){
 
 
-//        while (true){
-//            BufferedReader reader = new BufferedReader( new InputStreamReader(System.in));
-//           try {
-//               if(reader.readLine().contains("0")){
-//                   break;
-//               }
-//               ArrayList<Passenger> flightList = new ArrayList<>();
-//               System.out.println("Введите номер рейса :");
-//               int flightNum = Integer.parseInt(reader.readLine());
-//
-//               while (true){
-//                   if(reader.readLine().contains("stop")){
-//                       break;
-//                   }
-//                   System.out.println("Внесите данные пассажира для рейса - № " + flightNum);
-//                   String name = reader.readLine();
-//                   int doc = Integer.parseInt(reader.readLine());
-//                   flightList.add(new Passenger(name,doc,flightNum));
-//               }
-//               airport.put(flightNum,flightList);
-//           }catch (IOException e){
-//               e.printStackTrace();
-//           }
-//        }
-
-
-//        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//
-//        Flight flight = new Flight(1,new ArrayList<Passenger>());
-//        Flight flight1 = new Flight(2,new ArrayList<Passenger>());
-//        Flight flight2 = new Flight(3,new ArrayList<Passenger>());
-//        Flight flight3 = new Flight(4,new ArrayList<Passenger>());
-//
-//        ArrayList<Flight> flights = new ArrayList<>();
-//
-//
-//        while (true){
-//            try {
-//                System.out.println("'0' для остановки ввода пассажиров");
-//                if(reader.readLine().contains("0")){
-//                    break;
-//                }
-//                System.out.println("Введите имя пассажира:");
-//                String name = reader.readLine();
-//                System.out.println("Введите документы:");
-//                int docNum = Integer.parseInt(reader.readLine());
-//                System.out.println("Введите рейс пассажира:");
-//                int flightNum = Integer.parseInt(reader.readLine());
-//                switch (flightNum){
-//                    case 1:flight.getFlightList().add(new Passenger(name,docNum,flightNum));break;
-//                    case 2:flight1.getFlightList().add(new Passenger(name,docNum,flightNum));break;
-//                    case 3:flight2.getFlightList().add(new Passenger(name,docNum,flightNum));break;
-//                    case 4:flight3.getFlightList().add(new Passenger(name,docNum,flightNum));break;
-//                }
-//
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        flights.add(flight3);
-//        flights.add(flight1);
-//        flights.add(flight2);
-//        flights.add(flight);
-//
-//        for (Flight f:flights) {
-//            Collections.sort(f.getFlightList(), new FlightsCompare());
-//           airport.put(f.getFlightNum(),f);
-//        }
-//
-//        for(Map.Entry<Integer, Flight> item : airport.entrySet()){
-//            System.out.println(item.getKey());
-//            passengerList(item.getValue());
-//        }
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
@@ -126,19 +63,9 @@ public class Main {
         }
         Airport airport1 = new Airport(new TreeMap<Integer,LinkedList<Passenger>>());
 
-        for (Passenger p :passengersList) {
-           if(airport1.getAirportMap().containsKey(p.getFlight())) {
-                airport1.getAirportMap().get(p.getFlight()).add(p);
-            }else {
-               airport1.mapPut(p.getFlight(),p);
-           }
-        }
+        airport1.mapPut(passengersList);
 
-        for(Map.Entry<Integer,LinkedList<Passenger>> item : airport1.getAirportMap().entrySet()){
-            System.out.println(item.getKey());
-            Collections.sort(item.getValue(), new FlightsCompare());
-            passengerList(item.getValue());
-        }
+        printMap(airport1);
 
     }
 }
